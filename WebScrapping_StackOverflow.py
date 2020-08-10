55882cee6f104ed3f0430c0f6eee1775df041368
HocheolJang <45971175+No7beckham@users.noreply.github.com>
2020-08-10 18:13:59 +0900

    StackOverflow 스크래핑 시도
    중간에 잘 안되는 부분있어서 찾는중...

---
+++ WebScrapping_StackOverflow.py
@@ -0,0 +1,46 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = f"http://stackoverflow.com/jobs?q=python&sort=i"
+
+
+# 해야할 것
+# step1. 페이지 가져오기
+# step2. request 만들기
+# step3. Job 가져오기
+
+def get_last_page():
+    result = requests.get(URL)
+    soup = BeautifulSoup(result.text, "html.parser")
+    pages = soup.find("div", {"class": "s-pagination"}).find_all("a")
+    last_pages = pages[-2].get_text(strip=True)
+    return int(last_pages)
+
+
+def extract_job(html):
+    # 수업시간 작성한 코드
+    title = html.find("div", {"class": "fs-body3"}).find("h2").find("a")["title"]
+    # 어떤 분이 댓글 알려주신 코드
+    # title = html.find("h2").text.strip()
+    company, location = html.find("div", {"class": "-company"}).find_all("span", recursive=False)
+    company = company.get_text(strip=True).strip("\n")
+    location = company.get_text(strip=True).strip("-").strip("\n").strip(" \r")  # ("\n")과 (" \r") 둘 다 같은 뜻이다.
+    return {'title': title, 'company': company, 'location': location}
+
+
+def extract_jobs(last_page):
+    jobs = []
+    for page in range(last_page):
+        result = requests.get(f"{URL}&pg={page + 1}")
+        soup = BeautifulSoup(result.text, "html.parser")
+        results = soup.find_all("div", {"class": "s-link"})
+        for result in results:
+            job = extract_job(result)
+            jobs.append(job)
+    return jobs
+
+
+def get_jobs():
+    last_page = get_last_page()
+    jobs = extract_jobs(last_page)
+    return jobs
---
+++ WebScrapping_main.py
@@ -0,0 +1,11 @@
+from WebScrapping_indeed import get_jobs as get_indeed_jobs
+from WebScrapping_StackOverflow import get_jobs as get_so_jobs
+
+# indeed_jobs = get_indeed_jobs()
+so_jobs = get_so_jobs()
+
+# from indeed import extract_indeed_pages, extract_indeed_jobs
+# max_indeed_pages = extract_indeed_pages()
+
+
+
