7e2e9ca1a6d0367b3689923245b33a767cba5319
HocheolJang <45971175+No7beckham@users.noreply.github.com>
2020-08-15 03:40:51 +0900

    WebScraaping 강의

--- WebScrapping_StackOverflow.py
+++ WebScrapping_StackOverflow.py
@@ -17,20 +17,30 @@ def get_last_page():
     return int(last_pages)
 
 
+# def extract_job(html):
+#     # 수업시간 작성한 코드
+#     title = html.find("h2", {"class": "fs-body3"}).find("h2").find("a")["title"]
+#     # 어떤 분이 댓글 알려주신 코드
+#     # title = html.find("h2").text.strip()
+#     company, location = html.find("div", {"class": "-company"}).find_all("span", recursive=False)
+#     company = company.get_text(strip=True).strip("\n")
+#     location = company.get_text(strip=True).strip("-").strip("\n").strip(" \r")  # ("\n")과 (" \r") 둘 다 같은 뜻이다.
+#     return {'title': title, 'company': company, 'location': location}
+
 def extract_job(html):
-    # 수업시간 작성한 코드
-    title = html.find("div", {"class": "fs-body3"}).find("h2").find("a")["title"]
-    # 어떤 분이 댓글 알려주신 코드
-    # title = html.find("h2").text.strip()
-    company, location = html.find("div", {"class": "-company"}).find_all("span", recursive=False)
-    company = company.get_text(strip=True).strip("\n")
-    location = company.get_text(strip=True).strip("-").strip("\n").strip(" \r")  # ("\n")과 (" \r") 둘 다 같은 뜻이다.
-    return {'title': title, 'company': company, 'location': location}
+    title = html.find("h2").text.strip()
+    company, location = html.find("h3", {"class": "fc-black-700"}).find_all("span", recursive=False)
+    company = company.get_text(strip=True)
+    location = location.get_text(strip=True)
+    job_id = html('data-jobid')
+    return {'title': title, 'company': company, 'location': location,
+            "apply_link": f"https://stackoverflow.com/jobs/{job_id}"}
 
 
 def extract_jobs(last_page):
     jobs = []
     for page in range(last_page):
+        print(f"Scrapping StackOverflow: Page: {page}")
         result = requests.get(f"{URL}&pg={page + 1}")
         soup = BeautifulSoup(result.text, "html.parser")
         results = soup.find_all("div", {"class": "s-link"})
--- WebScrapping_main.py
+++ WebScrapping_main.py
@@ -1,8 +1,13 @@
 from WebScrapping_indeed import get_jobs as get_indeed_jobs
 from WebScrapping_StackOverflow import get_jobs as get_so_jobs
+from save import save_to_file
 
 # indeed_jobs = get_indeed_jobs()
 so_jobs = get_so_jobs()
+indeed_jobs = get_indeed_jobs()
+jobs = so_jobs + indeed_jobs
+save_to_file(jobs)
+
 
 # from indeed import extract_indeed_pages, extract_indeed_jobs
 # max_indeed_pages = extract_indeed_pages()
